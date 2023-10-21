b0b6807fbf8faa90e3ac5097b03ca2cfef234259
Marty <mtownley@stevens.edu>
2023-10-21 18:59:16 -0400

    project1 now computes rankings, creates recommendations, and writes to an output file

--- project1.m
+++ project1.m
@@ -6,11 +6,15 @@ close all;
 %readData;
 % Can also read a subset of the data - i.e. tracks only, training data only...
 
+outputFile = 'submission.csv';
+
 % Temporary for a quick test
-numUsers = 10;
+%numUsers = 10;
+numUsers = 20000;
 tracksPerUser = 6;
+wrOut = cell(tracksPerUser*numUsers+1,2);
+wrOut(1,1:2) = {'TrackID', 'Predictor'};
 % Rules based recommendation approach
-% This code hasn't been run yet, just putting ideas down
 for ii = 1:numUsers
   testUser = testSet(ii, 1);
   hits = zeros(1,tracksPerUser);
@@ -34,6 +38,9 @@ for ii = 1:numUsers
     % Associated data may include NaNs and -1s, so ignore those
     for kk = 1:length(assocData)
       elem = assocData(kk);
+      if elem == -1
+        break
+      end
       if ~isnan(elem) && elem ~= -1
         searchData = [testUser, elem];
         searchRows = find(trainData(:,1) == searchData(1));
@@ -43,9 +50,29 @@ for ii = 1:numUsers
       	    rating(jj) = rating(jj) + trainData(searchRows(search),3);
           end
         end
-
-        %hitIdx = find(trainData == searchData);
       end
     end
   end
+  % Rank by the average rating
+  avgRating = rating./hits;
+  for jj = 1:tracksPerUser
+    if hits(jj) == 0
+      avgRating(jj) = 0;
+    end
+  end
+  % Account for number of hits to skew the rankings slightly
+  avgRating = avgRating + (1e-3)*hits;
+  ranks = tiedrank(avgRating);
+  for jj = 1:tracksPerUser
+    testTrack = testSet(ii, jj+1);
+    outRow = (ii-1)*tracksPerUser+jj;
+    wrOut(outRow+1,1) = {strcat(num2str(testUser),'_',num2str(testTrack))};
+    if ranks(jj) > 3
+      wrOut(outRow+1,2) = {'1'};
+    else
+      wrOut(outRow+1,2) = {'0'};
+    end
+  end
 end
+
+writecell(wrOut,outputFile);
